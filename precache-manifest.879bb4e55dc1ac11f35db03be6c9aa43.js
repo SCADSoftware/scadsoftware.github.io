55d18351ab7179a6d5b176298ebde822c1c9e918
Andy Fensham <andy.fensham@gmail.com>
2019-09-11 12:26:34 +0200

    Vue PWA
    Vuew PWA

---
+++ precache-manifest.879bb4e55dc1ac11f35db03be6c9aa43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "02497c27c70155ffd6d0",
+    "url": "/js/chunk-vendors.e6f7c778.js"
+  },
+  {
+    "revision": "4ce65c8a5be700cc7455",
+    "url": "/js/app.ca3ced5d.js"
+  },
+  {
+    "revision": "8810e7d97691299a5b42",
+    "url": "/js/about.5c4bce17.js"
+  },
+  {
+    "revision": "bc08bbe95e04dd7cf1c90fe3e5cbcfa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4ce65c8a5be700cc7455",
+    "url": "/css/app.dde1df84.css"
+  }
+];
